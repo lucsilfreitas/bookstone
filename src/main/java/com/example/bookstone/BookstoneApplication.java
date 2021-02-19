9d8121f3d3b94f0b5aa31dfc39ac3f8a6cc78e11
LUCIANO DA SILVA FREITAS <lucsilfreitas@gmail.com>
2021-02-19 14:48:36 -0300

    Inicio

---
+++ src/main/java/com/example/bookstone/BookstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoneApplication.class, args);
+	}
+
+}
